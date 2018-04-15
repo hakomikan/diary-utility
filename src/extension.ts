5107e58a0925ce1d16fd6f60f26f9c8d3099944f
hakomikan <mail@s-taki.sakura.ne.jp>
2018-04-16 00:16:20 +0900

    modify template and move cursor

--- src/extension.ts
+++ src/extension.ts
@@ -14,21 +14,30 @@ export function activate(context: vscode.ExtensionContext) {
             vscode.commands.registerCommand(name, callback));
     }
 
+    function formatDate(date: Date) : string
+    {
+        return `${date.getFullYear()}-${PadLeft(`${date.getMonth()+1}`, "0", 2)}-${PadLeft(`${date.getDate()}`, "0", 2)}`;
+    }
+
     registerCommand('extension.CreateTodayDiary', () => {
         if (vscode.workspace.workspaceFolders !== undefined) {
             var workspace = vscode.workspace.workspaceFolders[0] as vscode.WorkspaceFolder;
-            var now = new Date(Date.now());
-            var targetPath = join(workspace.uri.fsPath, `${now.getFullYear()}-${PadLeft(`${now.getMonth()+1}`, "0", 2)}-${PadLeft(`${now.getDate()}`, "0", 2)}.md`);
+            var formatedToday = formatDate(new Date(Date.now()));
+            var targetPath = join(workspace.uri.fsPath, `${formatedToday}.md`);
 
             exists(targetPath, existsFile => {
                 if(!existsFile) {
-                    writeFile(targetPath, `${targetPath}`, err => { });
+                    writeFile(targetPath, `# ${formatedToday}\n- `, err => { });
                     vscode.window.showInformationMessage(`Create ${targetPath}!`);                        
                 }
             });
 
             vscode.workspace.openTextDocument(targetPath).then(doc => {
-                vscode.window.showTextDocument(doc);
+                vscode.window.showTextDocument(doc).then(editor => {
+                    var docEnd = doc.positionAt(doc.getText().length);
+                    editor.selection = new vscode.Selection(docEnd, docEnd);
+                    editor.revealRange(new vscode.Range(docEnd, docEnd), vscode.TextEditorRevealType.InCenter);
+                });
             });
         }
     });
